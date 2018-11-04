3c3bbbabec2b7b8e81dcbd90b355a2aa98de5b4a
Mason.Laptop <364206176@qq.com>
2018-11-04 15:21:54 +0800

    code 2 Next Greater Element I

---
+++ src/main/kotlin/com/mason/leetcode/bit/easy/SingleNumber136.kt
@@ -0,0 +1,52 @@
+package com.mason.leetcode.bit.easy
+
+/**
+ * Created by mwu on 2018/11/04
+ *
+ * Given a non-empty array of integers, every element appears twice except for one.
+ * Find that single one.
+ * Note:
+ * Your algorithm should have a linear runtime complexity. Could you implement it without using extra memory?
+ *
+ * Example 1:
+ * Input: [2,2,1]
+ * Output: 1
+ *
+ * Example 2:
+ * Input: [4,1,2,1,2]
+ * Output: 4
+ */
+object SingleNumber136 {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val nums = intArrayOf(4, 1, 2, 1, 2)
+    val result = singleNumber(nums)
+    println(result)
+  }
+
+  private fun singleNumber(nums: IntArray): Int {
+    // 再也不做空判断了。。。尼玛，这么影响性能的
+//    if (nums.isEmpty()) return 0
+//    var result = 0
+//    (0 until nums.size).forEach { it ->
+//      result = result xor nums[it]
+//    }
+//    return result
+
+    val countMap = hashMapOf<Int, Int>()
+    nums.forEach {
+      val value = countMap[it]
+      if (value == null) {
+        countMap[it] = 1
+      } else {
+        var temp = countMap[it]
+        if (temp != null) {
+          temp++
+          countMap[it] = temp
+        }
+      }
+    }
+    return countMap.entries.find { it.value == 1 }?.key ?: 0
+  }
+}
---
+++ src/main/kotlin/com/mason/leetcode/stack/easy/NextGreaterElementI496.kt
@@ -0,0 +1,79 @@
+package com.mason.leetcode.stack.easy
+
+import com.mason.leetcode.tools.printIntArray
+import java.util.ArrayDeque
+import java.util.Deque
+import java.util.HashMap
+
+
+/**
+ * Created by mwu on 2018/11/04
+ *
+ *  You are given two arrays (without duplicates) nums1 and nums2 where nums1’s elements are subset of nums2.
+ *  Find all the next greater numbers for nums1's elements in the corresponding places of nums2.
+ *  The Next Greater Number of a number x in nums1 is the first greater number to its right in nums2.
+ *  If it does not exist, output -1 for this number.
+ *
+ *  Example 1:
+ *  Input: nums1 = [4,1,2], nums2 = [1,3,4,2].
+ *  Output: [-1,3,-1]
+ *  Explanation:
+ *  For number 4 in the first array, you cannot find the next greater number for it in the second array, so output -1.
+ *  For number 1 in the first array, the next greater number for it in the second array is 3.
+ *  For number 2 in the first array, there is no next greater number for it in the second array, so output -1.
+ *
+ *  Example 2:
+ *  Input: nums1 = [2,4], nums2 = [1,2,3,4].
+ *  Output: [3,-1]
+ *  Explanation:
+ *  For number 2 in the first array, the next greater number for it in the second array is 3.
+ *  For number 4 in the first array, there is no next greater number for it in the second array, so output -1.
+ *
+ *  Note:
+ *  All elements in nums1 and nums2 are unique.
+ *  The length of both nums1 and nums2 would not exceed 1000.
+ */
+object NextGreaterElementI496 {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val nums1 = intArrayOf(2, 4)
+    val nums2 = intArrayOf(1, 2, 3, 4)
+    val result = nextGreaterElement(nums1, nums2)
+    printIntArray(result)
+  }
+
+  private fun nextGreaterElement(nums1: IntArray, nums2: IntArray): IntArray {
+//    val result = IntArray(size = nums1.size, init = { -1 })
+//    val index = IntArray(size = nums1.size)
+//    nums1.forEachIndexed { outer, ov ->
+//      nums2.forEachIndexed { inner, iv ->
+//        if (ov == iv) index[outer] = inner
+//      }
+//    }
+//    nums1.forEachIndexed { outer, ov ->
+//      (index[outer] until nums2.size).forEach { inner ->
+//        if (ov < nums2[inner]) {
+//          result[outer] = nums2[inner]
+//          return@forEachIndexed
+//        }
+//      }
+//    }
+//    return result
+
+    val map = HashMap<Int, Int>()
+    val result = IntArray(nums1.size)
+    val stack = ArrayDeque<Int>()
+    for (num2 in nums2) {
+      while (!stack.isEmpty() && stack.peek() < num2) {
+        map[stack.removeFirst()] = num2
+      }
+      stack.addFirst(num2)
+    }
+    for (i in 0 until nums1.size) {
+      result[i] = map.getOrDefault(nums1[i], -1)
+    }
+
+    return result
+  }
+}
