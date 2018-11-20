6a34afa4478db09605ef5d87bb3d380d8da2c0af
MasonEcnu <364207187@qq.com>
2018-11-20 15:51:02 +0800

    test

---
+++ src/main/kotlin/com/mason/leetcode/string/easy/LongestCommonPrefix14.kt
@@ -0,0 +1,41 @@
+package com.mason.leetcode.string.easy
+
+/**
+ * Created by mwu on 2018/11/20
+ *
+ * Write a function to find the longest common prefix string amongst an array of strings.
+ * If there is no common prefix, return an empty string "".
+ *
+ * Example 1:
+ * Input: ["flower","flow","flight"]
+ * Output: "fl"
+ *
+ * Example 2:
+ * Input: ["dog","racecar","car"]
+ * Output: ""
+ * Explanation: There is no common prefix among the input strings.
+ *
+ * Note:
+ * All given inputs are in lowercase letters a-z.
+ *
+ */
+object LongestCommonPrefix14 {
+
+  @JvmStatic
+  fun main(args: Array<String>) {
+    val strs = arrayOf("flower", "flow", "flight")
+    val result = longestCommonPrefix(strs)
+    println(result)
+  }
+
+  private fun longestCommonPrefix(strs: Array<String>): String {
+    if (strs.isEmpty()) return ""
+    var prefix = strs[0]
+    for (i in 1 until strs.size)
+      while (strs[i].indexOf(prefix) != 0) {
+        prefix = prefix.substring(0, prefix.length - 1)
+        if (prefix.isEmpty()) return ""
+      }
+    return prefix
+  }
+}
